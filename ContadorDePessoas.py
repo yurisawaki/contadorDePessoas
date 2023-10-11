3edb347f57c843be7117b8f8d3c9364352b87907
yurisawaki <141770852+yurisawaki@users.noreply.github.com>
2023-10-11 13:04:26 -0300

    Update ContadorDePessoas.py

--- ContadorDePessoas.py
+++ ContadorDePessoas.py
@@ -1,9 +1,5 @@
 import cv2
-import numpy as np
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import paho.mqtt.client as mqtt
-import time
 
 def center(x, y, w, h):
     x1 = int(w / 2)
@@ -12,119 +8,130 @@ def center(x, y, w, h):
     cy = y + y1
     return cx, cy
 
-
-MQTT_BROKER = "192.168.100.42"  
+# Configurar informações de conexão MQTT
+MQTT_BROKER = "broker.emqx.io"  # Substitua pelo endereço IP ou nome do host do seu broker MQTT
 MQTT_PORT = 1883
-MQTT_TOPIC = "contagem_de_pessoas"
+MQTT_TOPIC1 = "/UFPA/LAAI/contagem_de_pessoas/entrada/"
+MQTT_TOPIC2 = "/UFPA/LAAI/contagem_de_pessoas/saida/"
 
-
+# Variáveis para contar as pessoas
 pessoas = 0
 
-# callback
 def on_publish(client, userdata, mid):
-    print("Mensagem publicada com sucesso")
+   print("mensagem publicada")
 
-# 
+# Função de callback quando o cliente MQTT é desconectado
 def on_disconnect(client, userdata, rc):
     if rc != 0:
-        print("Tentando reconectar...")
+        print("Desconexão inesperada. Tentando reconectar...")
         client.reconnect()
 
-# Cliente MQTT
+# Configurar o cliente MQTT
 client = mqtt.Client()
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
+client.connect(MQTT_BROKER, MQTT_PORT, 120)
 client.on_publish = on_publish
-client.on_disconnect = on_disconnect  
+client.on_disconnect = on_disconnect  # Configurar a função de callback de desconexão
 
-
-camera = PiCamera()
-camera.resolution = (640, 480)
-raw_capture = PiRGBArray(camera, size=(640, 480))
+# Inicializa a webcam
+cap = cv2.VideoCapture(0)  # 0 é o índice da webcam padrão, você pode alterá-lo conforme necessário
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 posL = 150
 offset = 20
-
-xy1 = (0, posL)  
-
+xy1 = (0, posL) 
 detects = []
 
+
 total = 0
-up = 0
-down = 0
+entrada = 0
+saida = 0
 
-for frame in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
-    image = frame.array
-
-    xy2 = (image.shape[1], posL)
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    fgbmask = fgbg.apply(gray)
-
-    retval, th = cv2.threshold(fgbmask, 200, 255, cv2.THRESH_BINARY)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-
-    opening = cv2.morphologyEx(th, cv2.MORPH_OPEN, kernel, iterations=2)
-
-    dilatation = cv2.dilate(opening, kernel, iterations=8)
-
-    closing = cv2.morphologyEx(dilatation, cv2.MORPH_CLOSE, kernel, iterations=8)
-
-    countours, hierarchy = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    i = 0
-    for cnt in countours:
-        (x, y, w, h) = cv2.boundingRect(cnt)
-        area = cv2.contourArea(cnt)
-
-        detect = []
-
-        if int(area) < 11000 or int(area) < 20000:
-            centro = center(x, y, w, h)
-
-            if len(detects) <= i:
-                detects.append([])
-
-            if centro[1] > posL - offset and centro[1] < posL + offset:
-                detects[i].append(centro)
-            else:
-                detects[i].clear()
-
-            i += 1
-
-    if i == 0:
-        detects.clear()
-
-    if len(countours) == 0:
-        for detect in detects:
-            detects.clear()
-    else:
-        for detect in detects:
-            if len(detect) > 0:
-                for c in range(1, len(detect)):
-                    if detect[c - 1][1] < posL and detect[c][1] > posL:
-                        detect.clear()
-                        up += 1
-                        total += 1
-                        continue
-
-                    if detect[c - 1][1] > posL and detect[c][1] < posL:
-                        detect.clear()
-                        down += 1
-                        total += 1
-                        continue
-
-    log_info = f"Total: {total}, Subindo: {up}, Descendo: {down}"
-    print(log_info)
-
-    # Publicar a contagem de pessoas 
+while True:
     try:
-        client.publish(MQTT_TOPIC, log_info)
-    except (ConnectionError, OSError):
-        print("Reconectando...")
-        client.reconnect()
+        ret, frame = cap.read()
 
-    raw_capture.truncate(0)
+        xy2 = (frame.shape[1], posL)  # Extremidade direita da tela (largura do quadro)
+        
+        cv2.line(frame, xy1, xy2, (255, 0, 0), 3)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        fgbmask = fgbg.apply(gray)
+
+        retval, th = cv2.threshold(fgbmask, 200, 255, cv2.THRESH_BINARY)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+
+        opening = cv2.morphologyEx(th, cv2.MORPH_OPEN, kernel, iterations=2)
+
+        dilatation = cv2.dilate(opening, kernel, iterations=8)
+
+        closing = cv2.morphologyEx(dilatation, cv2.MORPH_CLOSE, kernel, iterations=8)
+
+        countours, hierarchy = cv2.findContours(closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        i = 0
+        for cnt in countours:
+            (x, y, w, h) = cv2.boundingRect(cnt)
+            area = cv2.contourArea(cnt)
+
+            detect = []
+
+            if int(area) < 11000 or int(area) < 20000:
+                centro = center(x, y, w, h)
+
+                if len(detects) <= i:
+                     detects.append([])
+
+                if centro[1] > posL - offset and centro[1] < posL + offset:
+                    detects[i].append(centro)
+                    
+                    # Desenhar um retângulo ao redor da pessoa detectada
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    
+                else:
+                    detects[i].clear()
+
+                i += 1
+
+        if i == 0:
+            detects.clear()
+
+        if len(countours) == 0:
+            for detect in detects:
+                detects.clear()
+        else:
+            for detect in detects:
+                if len(detect) > 0:
+                    for c in range(1, len(detect)):
+                        if detect[c - 1][1] < posL and detect[c][1] > posL:
+                            detect.clear()
+                            entrada += 1
+                            total += 1
+                            # Publicar a contagem de pessoas no tópico MQTT
+                            info_entrada = f"Entrada: {entrada}"
+                            client.publish(MQTT_TOPIC1, info_entrada)
+                            continue
+
+                        if detect[c - 1][1] > posL and detect[c][1] < posL:
+                            detect.clear()
+                            saida += 1
+                            total += 1
+                            info_saida = f"Saída: {saida}"
+                            client.publish(MQTT_TOPIC2, info_saida)
+                            continue
+
+        # Mostrar a janela de visualização do vídeo
+        cv2.imshow("Visualização da Webcam", frame)
+
+        # Se pressionar a tecla 'q', sair do loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    except (ConnectionError, OSError):
+         print("Erro ao publicar. Reconectando...")
+
+# Liberar a webcam e fechar a janela de visualização
+cap.release()
+cv2.destroyAllWindows()
